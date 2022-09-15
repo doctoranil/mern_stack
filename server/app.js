ec7fb65bec42ff42b590c121ff3636d7dc959de0
anil <anil15124@gmail.com>
2022-09-15 13:38:07 +0530

    add files first time

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const dotenv= require("dotenv");
+dotenv.config({path:'./config.env'});
+const mongoose= require('mongoose');
+const express= require('express');
+require('./db/conn');
+const PORT= process.env.PORT;
+const app= express();
+
+app.use(express.json());
+app.use(require('./router/auth'));
+
+ const middleware=(req,res,next)=>{
+    console.log("Hello my middleware");
+
+    next();
+ }
+
+
+app.get('/',(req,res)=>{
+    res.send(`Hello World from the server`);
+
+});
+
+app.get('/about',middleware,(req,res)=>{
+    res.send(`Hello World about page`);
+
+});
+
+app.get('/contact',(req,res)=>{
+    res.send(`Hello World contact page`);
+
+});
+
+app.get('/signin',(req,res)=>{
+    res.send(`Hello World sign in page`);
+
+});
+
+app.get('/signup',(req,res)=>{
+    res.send(`Hello World signup page`);
+
+});
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port number ${PORT}`);
+});
