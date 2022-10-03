a612acc811f93a3f4ad31559a3304fce026448af
anil <anil15124@gmail.com>
2022-10-03 12:04:49 +0530

    adding react components

---
+++ client/src/components/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <>
+        <section className='signup'>
+        <div>
+            
+        </div>
+
+        </section>
+    </>
+  )
+}
+
+export default Signup
