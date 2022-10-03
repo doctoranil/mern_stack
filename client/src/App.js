8a863b04b5b8b15ed449f6a24baa28b75bfd04b0
anil <anil15124@gmail.com>
2022-10-03 12:03:24 +0530

    react add changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import About from './components/About'
+import Contact from './components/Contact'
+import Login from './components/Login'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import { Routes,Route,Link  } from "react-router-dom";
 function App() {
   return (
-    <div>Hello Word</div>
+    <>
+    <Navbar />
+   
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+
+
+
+   
+   
+    </>
+  
+
   )
 }
 
